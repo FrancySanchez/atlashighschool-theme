38eeae11fca8a06aad3b84236906dbfa57c277c5
juanrules <juanrules@gmail.com>
2016-10-09 21:40:49 +0100

    New search template

---
+++ template-schools-search.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Schools search Builder
+ */
+
+$data = Timber::get_context();
+// $data['page'] = Timber::get_post();
+$args = array(
+	'post_type' => 'schools',
+);
+$data['schools'] = Timber::get_posts($args);
+
+foreach ($data['schools'] as $key => $school) {
+	
+	
+	$school['imagen'] = new TimberImage($schoolID);
+};
+
+
+
+Timber::render('twigs/schools-search.twig', $data);
+
+?>
