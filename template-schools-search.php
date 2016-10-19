176e4ebf93441a6bfa0ab78bce6d6495421234f6
juanrules <juanrules@gmail.com>
2016-10-19 20:41:17 +0100

    Bringing in the images to the school cards

--- template-schools-search.php
+++ template-schools-search.php
@@ -9,9 +9,16 @@ $args = array(
 	'post_type' => 'schools',
 );
 $data['schools'] = Timber::get_posts($args);
+
+foreach ($data['schools'] as $key => $school) {
+	 $school->featured_image = new TimberImage($school->background_school);
+}
+
 $data['acfs'] = get_fields();
 
 Timber::render('twigs/panel-builder.twig', $data);
 Timber::render('twigs/schools-search.twig', $data);
+
+
 
 ?>
