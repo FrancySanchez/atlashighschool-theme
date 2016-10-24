0f950ad1acc6a65853d0d5f47ba96e0e92137336
FrancySanchez <francy.sanchez@gmail.com>
2016-10-24 14:25:01 +0100

    Pages + fonts
    * accommodation page
    *gallery
    *fonts

--- templates/head.php
+++ templates/head.php
@@ -4,4 +4,5 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
   <link rel="stylesheet" id="font-awesome-css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css?ver=4.4.5" type="text/css" media="all">
+  <link href="https://fonts.googleapis.com/css?family=Anton|Open+Sans" rel="stylesheet">
 </head>
